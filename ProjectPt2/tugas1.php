0a0b26cc711c18ecb3bc273545a0adbdda497bb0
Kizuxy <berliferizadam16@gmail.com>
2022-02-12 10:17:13 +0700

    2nd

--- ProjectPt2/tugas1.php
+++ ProjectPt2/tugas1.php
@@ -8,26 +8,31 @@
     <title>Document</title>
 </head>
 <body>
-    <table>
+    <center>
+        <table border = 1 cellspacing = 1>
+    <?php
 
-<?php
-
-$index = [
-    ["NIS", "Nama", "Kelas", "Bahasa Indonesia", "Bahasa Inggris", "Matematika", "Produktif", "Total Nilai", "Rata-rata", "Status"],
-    ["1018", "Berli", "XI RPL 1", 95, 80, 78, 78, 331, 82.75, "Lulus"]
-];
-
-for ($row = 0; $row < 2; $row++) {
-    echo "<tr>";
-    for ($col = 0; $col < 9; $col++) {
-        echo "<td>".$index[$row][$col]."</td>";
-         
-    }
-    echo "</tr>";
-} 
-?>
-
-    </table>
+    $judul = ["Judul" => "Data Siswa Kelas XI RPL"];
+    echo "<h2>".$judul["Judul"]."</h2>";
+    
+    $index = [
+        ["NIS", "Nama", "Kelas", "Bahasa Indonesia", "Bahasa Inggris", "Matematika", "Produktif", "Total Nilai", "Rata-rata", "Status"],
+        ["1018", "Berli", "XI RPL 1", 95, 80, 78, 78, 331, 82.75, "Lulus"]
+    ];
+    
+    for ($row = 0; $row < 2; $row++) {
+        echo "<tr>";
+        for ($col = 0; $col < 9; $col++) {
+            echo "<td>".$index[$row][$col]."</td>";
+             
+        }
+        echo "</tr>";
+    } 
+    ?>
+    
+        </table>
+        
+    </center>
     
 </body>
 </html>
